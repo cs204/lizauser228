02ff16f9bffcfae6e27655615b314cbc8bc5703d
test <test>
2023-10-10 21:55:52 +0700

    test

---
+++ extensions.py
@@ -0,0 +1,11 @@
+fileName = input('File name: ')
+extension = fileName.split('.')[-1]
+
+match extension:
+    case 'gif': print('image/gif')
+    case 'jpg' | 'jpeg': print('image/jpeg')
+    case 'png': print('image/png')
+    case 'pdf': print('application/pdf')
+    case 'txt': print('text/plain')
+    case 'zip': print('application/zip')
+    case _: print('application/octet-stream')
