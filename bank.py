617ed33b509f7f9cceae2abc1c591ac6a94b15f2
test <test>
2023-10-10 21:37:59 +0700

    test

---
+++ bank.py
@@ -0,0 +1,7 @@
+greeting = input("Приветствие: ")
+if greeting.lower().startswith("здравствуйте"):
+    print("$0")
+elif greeting.lower().startswith("з"):
+    print("$20")
+else:
+    print("$100")
